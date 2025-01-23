99b48c24da5f3f409da73b33ff0020dae33b2719
GilbertoGoncalves21 <gibagonjunior@gmail.com>
2025-01-23 14:52:57 -0300

    Fix - Correção para a iniciação da tela ocorrer corretamente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)
   }
 ];
